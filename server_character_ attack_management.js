60e58df2ab02c8dcbb2e5a2c5af47f025cde8517
Maffittaa <mafaldam2001@outlook.com>
2024-04-24 00:47:48 +0100

    attack from both players done, starting the turns

--- server_character_ attack_management.js
+++ server_character_ attack_management.js
@@ -3,54 +3,57 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const connectionOptions = {
+const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'Acoteias2015!',
     database: 'ttdd_database'
-};
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log("Error connection to DB: " + err);
+        return;
+    }
+    console.log("Connected to database");
+})
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("www"));
 
-app.post('/attack', (req, res) => { // character 1 from player attacking the character 1 from player 2
+app.post('/attack1', (req, res) => { // attack from player 1 to opponent // attack from player 2 to opponent
     var attackerSlot = req.body.attackerSlot;
     var targetSlot = req.body.targetSlot;
-    var player1_id = req.body.player1_id;
+    var playerID = req.body.player_id;
     var match_id = req.body.match_id;
-    var connection = mysql.createConnection(connectionOptions);
-    console.log(attackerSlot);
-    console.log(targetSlot);
-    connection.connect();
+    // console.log(req.body);
+    // console.log("attacker", req.body.attackerSlot);
+    // console.log("target", req.body.targetSlot);
+    turns(match_id, playerID);
+    console.log(turns(match_id, playerID));
 
-    connection.query("SELECT caracter_id, caracter_attack FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + player1_id + " AND player_match_character_tile_id = " + attackerSlot, //select a character from player to attack
+    connection.execute("SELECT caracter_id, caracter_attack FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + playerID + " AND player_match_character_match_id = " + match_id + " AND player_match_character_tile_id = " + attackerSlot, //select a character from player to attack
         function (error, rows, fields) {
             if (error) {
                 res.send(error);
             } else {
                 var attackDamage = rows[0].caracter_attack;
-                connection.query("UPDATE playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id SET player_match_character_character_current_HP = player_match_character_character_current_HP - " + attackDamage + " WHERE player_match_character_match_id = " + match_id + " AND player_match_character_tile_id = " + targetSlot, //if all goes well, after that we select a character from the opponent to be attacked
+                connection.execute("UPDATE playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id SET player_match_character_character_current_HP = player_match_character_character_current_HP - " + attackDamage + " WHERE player_match_character_match_id = " + match_id + " AND player_match_character_player_id <> " + playerID + " AND player_match_character_tile_id = " + targetSlot, //if all goes well, after that we select a character from the opponent to be attacked
                     function (error, rows, fields) {
                         if (error) {
-
                             res.send(error);
                         } else {
                             res.send(rows);
                         }
-                        connection.end();
                     });
             }
         });
 
 });
 
-
-app.get('/resetHPCharacters', (req, res) => { //reset HP of character from player 2
+app.get('/resetHPCharacters', (req, res) => { //reset HP of characters from each match
     var match_id = req.query.match_id;
-    var connection = mysql.createConnection(connectionOptions);
-    connection.connect();
-
-    connection.query("UPDATE playermatchcharacter SET player_match_character_character_current_HP = 10 WHERE player_match_character_match_id = " + match_id,
+    connection.execute("UPDATE playermatchcharacter SET player_match_character_character_current_HP = 10 WHERE player_match_character_match_id = " + match_id,
         function (error, rows, fields) {
             if (error) {
                 res.send(error);
@@ -60,74 +63,16 @@ app.get('/resetHPCharacters', (req, res) => { //reset HP of character from playe
         });
 });
 
-
+function turns(matchID, playerID) {
+    if (!matchID || !playerID) {
+        return;
+    } else {
+        turn = playerID;
+        return true;
+    }
+}
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
-
-// app.get('/getCharacterFromPlayer1', (req, res) => { //retrieve characters from player 1
-//     var player1_id = req.query.player1_id
-//     var connection = mysql.createConnection(connectionOptions);
-//     connection.connect();
-
-//     connection.query("SELECT caracter_name, caracter_attack, caracter_id FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + player1_id,
-//         function (error, rows, fields) {
-//             if (error) {
-//                 res.send(error);
-//             } else {
-//                 res.send(rows);
-//             }
-//         });
-//     connection.end();
-
-// });
-
-// app.get('/getCharacterFromPlayer2', (req, res) => { // retrieve characters from player 2
-//     var player2_id = req.query.player2_id;
-//     var connection = mysql.createConnection(connectionOptions);
-//     connection.connect();
-
-//     connection.query("SELECT caracter_name, caracter_attack, caracter_id FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + player2_id,
-//         function (error, rows, fields) {
-//             if (error) {
-//                 res.send(error);
-//             } else {
-//                 res.send(rows);
-//             }
-//         });
-//     connection.end();
-
-// });
-
-
-// app.get('/attackWithCharacter1', (req, res) => {
-//     var player1_id = req.query.player1_id;
-//     var connection = mysql.createConnection(connectionOptions); //connect to mysql
-//     connection.connect();
-
-//     connection.query("UPDATE playermatchcharacter SET player_match_character_character_current_HP = player_match_character_character_current_HP - 8 WHERE player_match_character_player_id = " + player1_id,
-//         function (error, rows, fields) {
-//             if (error) {
-//                 res.send(error);
-//             } else {
-//                 res.send(rows);
-//             }
-//         });
-// });
-
-// app.get('/attackWithCharacter2', (req, res) => {
-//     var player2_id = req.query.player2_id;
-//     var connection = mysql.createConnection(connectionOptions); //connect to mysql
-//     connection.connect();
-
-//     connection.query("UPDATE playermatchcharacter SET player_match_character_character_current_HP = player_match_character_character_current_HP - 3 WHERE player_match_character_player_id = " + player2_id,
-//         function (error, rows, fields) {
-//             if (error) {
-//                 res.send(error);
-//             } else {
-//                 res.send(rows);
-//             }
-//         });
-// });
 
 
--- www/client.js
+++ www/client.js
@@ -1,35 +1,34 @@
 var attackerSlot = 0
 var targetSlot = 0
-function setAttackerSlot(slot) { //attacker has a slot now
+function setAttackerSlot(slot) { //attacker has a slot 
 
     attackerSlot = slot;
 }
 
-function setTargetSlot(slot) { // if attacker has a slot, target has a slot now
+function setTargetSlot(slot) { // if attacker has a slot, target has a slot 
     if (attackerSlot != 0) {
         targetSlot = slot;
-        doAttack();
+        doAttack1();
         attackerSlot = 0;
         targetSlot = 0;
     }
 }
 
-function doAttack() { // passing the attackerslot and the target slot with the player1_id to the server
-    var player1_id = document.getElementById("player1_id").value;
-    var player2_id = document.getElementById("player2_id").value;
+function doAttack1() { // passing the attackerslot and the target slot with the player_id and the match_id to the server
+    var player_id = document.getElementById("player_id").value;
     var match_id = document.getElementById("match_id").value;
     $.ajax({
         type: 'POST',
-        url: '/attack',
+        url: '/attack1',
         data: {
-            "player1_id": player1_id,
+            "player_id": player_id,
             "match_id": match_id,
             "attackerSlot": attackerSlot,
             "targetSlot": targetSlot
         },
         success: function (data) {
             console.log(data);
-            document.getElementById("result").innerHTML = JSON.stringify(data);
+            document.getElementById("result").innerHTML = "Oponent Atacked";
         },
         error: function (err) {
             console.log(err);
@@ -56,148 +55,3 @@ function resetHPFromCharacters() {
     })
 }
 
-// function getCharactersFromPlayer1() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/getCharacterFromPlayer1',
-//         data: {
-//             "player1_id": player1_id,
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result5").innerHTML = "Your characters are: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-// function getCharactersFromPlayer2() {
-//     var player2_id = document.getElementById('player2_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/getCharacterFromPlayer2',
-//         data: {
-//             "player2_id": player2_id
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result6").innerHTML = "Your characters are: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-
-// function character2() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/character2FromPlayer',
-//         data: {
-//             "player1_id": player1_id
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result7").innerHTML = "You have selected: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-// function character3() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/character3FromPlayer',
-//         data: {
-//             "player1_id": player1_id
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result7").innerHTML = "You have selected: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-// function character4() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/character4FromPlayer',
-//         data: {
-//             "player1_id": player1_id
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result7").innerHTML = "You have selected: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-// function character5() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/character5FromPlayer',
-//         data: {
-//             "player1_id": player1_id
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result7").innerHTML = "You have selected: " + JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-
-// function attackFromCharacter1() {
-//     var player1_id = document.getElementById('player1_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/attackWithCharacter1',
-//         data: {
-//             "player1_id": player1_id,
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result5").innerHTML = JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
-
-// function attackFromCharacter2() {
-//     var player2_id = document.getElementById('player2_id').value;
-//     $.ajax({
-//         type: 'GET',
-//         url: '/attackWithCharacter2',
-//         data: {
-//             "player2_id": player2_id,
-//         },
-//         success: function (data) {
-//             console.log(data);
-//             document.getElementById("result6").innerHTML = JSON.stringify(data);
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     })
-// }
