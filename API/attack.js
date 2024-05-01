8265237741e70709dd16efc20fc828db0e20e4f6
Maffittaa <mafaldam2001@outlook.com>
2024-05-01 17:22:26 +0100

    turns completly working. Close range characters can only attack other close range characters. Long range characters can attack every character

--- API/attack.js
+++ API/attack.js
@@ -3,12 +3,16 @@ const connection = require('../database');
 const router = express.Router();
 
 router.post('/attack', (req, res) => { // attack from player 1 to opponent // attack from player 2 to opponent
+    checkPlayerTurn(req, res, doAttack);
+});
+
+function doAttack(req, res, isPlayerTurn) { //attack after checking if it's the player's turn
     var attackerSlot = req.body.attackerSlot;
     var targetSlot = req.body.targetSlot;
     var playerID = req.body.player_id;
     var match_id = req.body.match_id;
-    const isItPlayersTurn = isPlayerTurn(res, match_id, playerID);
-    if (!isItPlayersTurn) {
+    if (!isPlayerTurn) {
+        res.send("Not your turn yet..");
         return;
     } else {
         connection.execute("SELECT caracter_id, caracter_attack FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + playerID + " AND player_match_character_match_id = " + match_id + " AND player_match_character_tile_id = " + attackerSlot, //select a character from player to attack
@@ -16,6 +20,12 @@ router.post('/attack', (req, res) => { // attack from player 1 to opponent // at
                 if (error) {
                     res.send(error);
                 } else {
+                    if (attackerSlot == 1 || attackerSlot == 2 || attackerSlot == 3) {
+                        if (targetSlot == 4 || targetSlot == 5) {
+                            console.log("Can't attack that target");
+                            return;
+                        }
+                    }
                     var attackDamage = rows[0].caracter_attack;
                     connection.execute("UPDATE playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id SET player_match_character_character_current_HP = player_match_character_character_current_HP - " + attackDamage + " WHERE player_match_character_match_id = " + match_id + " AND player_match_character_player_id <> " + playerID + " AND player_match_character_tile_id = " + targetSlot, //if all goes well, after that we select a character from the opponent to be attacked
                         function (error, rows, fields) {
@@ -24,30 +34,27 @@ router.post('/attack', (req, res) => { // attack from player 1 to opponent // at
                             } else {
                                 res.send(rows);
                             }
-                        });
+                        })
                 }
-            });
+            })
     }
-});
-
+}
 
 router.get('/endTurn', (req, res) => { //ends the turn and passes to the other player
     var match_id = req.query.match_id;
     var playerID = req.query.player_id;
 
-    connection.execute("SELECT matche_player1_id as player, matche_player2_id as opponent FROM matche WHERE matche_id = " + match_id,
+    connection.execute("SELECT matche_player1_id, matche_player2_id FROM matche WHERE matche_id = " + match_id,
         function (error, rows, fields) {
             if (error) {
                 res.send(error);
             } else {
-                var playerTurn = rows[0].player;
-                var opponentTurn = rows[0].opponent;
                 var nextPlayer;
-                if (playerID == playerTurn) {
-                    nextPlayer = opponentTurn;
+                if (playerID == rows[0].matche_player1_id) {
+                    nextPlayer = rows[0].matche_player2_id;
                     console.log("Turn Switched!");
-                } else if (playerID == opponentTurn) {
-                    nextPlayer = playerTurn;
+                } else if (playerID == rows[0].matche_player2_id) {
+                    nextPlayer = rows[0].matche_player1_id;
                     console.log("Turn Switched!");
                 }
                 connection.execute("UPDATE matche SET matche_turn_player_id = " + nextPlayer + " WHERE matche_id = " + match_id,
@@ -64,6 +71,19 @@ router.get('/endTurn', (req, res) => { //ends the turn and passes to the other p
 });
 
 
+function checkPlayerTurn(req, res, callback) { //is it the player's turn?
+    var player_id = req.body.player_id;
+    var match_id = req.body.match_id;
+    connection.execute("SELECT COUNT(*) as count FROM matche where matche_id = ? AND matche_turn_player_id = ? ", [match_id, player_id],
+        function (error, rows, fields) {
+            if (error || rows[0].count == 0) {
+                callback(req, res, false);
+            } else {
+                callback(req, res, true);
+            }
+        });
+};
+
 router.get('/resetHPCharacters', (req, res) => { //reset HP of characters from each match
     var match_id = req.query.match_id;
     connection.execute("UPDATE playermatchcharacter SET player_match_character_character_current_HP = 10 WHERE player_match_character_match_id = ?", [match_id],
@@ -75,27 +95,6 @@ router.get('/resetHPCharacters', (req, res) => { //reset HP of characters from e
             }
         });
 });
-
-function isPlayerTurn(res, matchID, playerID) { //is it the player's turn?
-    if (!matchID || !playerID) {
-        console.log("No match or player found");
-        return;
-    }
-
-    connection.execute("SELECT COUNT(*) as count FROM matche where matche_id = ? AND matche_turn_player_id = ? ", [matchID, playerID],
-        function (error, rows, fields) {
-            if (error) {
-                return res.send(error);
-            }
-            var turn = rows[0].count;
-            if (rows && turn > 0) {
-                res.send(rows);
-                console.log("It's your turn!");
-            } else {
-                console.log("It's not your turn yet...");
-            }
-        });
-};
 
 module.exports = router;
 
