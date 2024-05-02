4f7abef576831a27ea638ffd10ad137e30b75ab1
Maffittaa <mafaldam2001@outlook.com>
2024-05-02 13:32:41 +0100

    attack is completly done. Added the script to create the database

--- API/attack.js
+++ API/attack.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connection = require('../database');
 const router = express.Router();
 
+var alreadyAttacked = false; //variable to check if a character has already attacked or not
+
 router.post('/attack', (req, res) => { // attack from player 1 to opponent // attack from player 2 to opponent
     checkPlayerTurn(req, res, doAttack);
 });
@@ -15,28 +17,36 @@ function doAttack(req, res, isPlayerTurn) { //attack after checking if it's the 
         res.send("Not your turn yet..");
         return;
     } else {
-        connection.execute("SELECT caracter_id, caracter_attack FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + playerID + " AND player_match_character_match_id = " + match_id + " AND player_match_character_tile_id = " + attackerSlot, //select a character from player to attack
-            function (error, rows, fields) {
-                if (error) {
-                    res.send(error);
-                } else {
-                    if (attackerSlot == 1 || attackerSlot == 2 || attackerSlot == 3) {
-                        if (targetSlot == 4 || targetSlot == 5) {
-                            console.log("Can't attack that target");
-                            return;
-                        }
-                    }
-                    var attackDamage = rows[0].caracter_attack;
-                    connection.execute("UPDATE playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id SET player_match_character_character_current_HP = player_match_character_character_current_HP - " + attackDamage + " WHERE player_match_character_match_id = " + match_id + " AND player_match_character_player_id <> " + playerID + " AND player_match_character_tile_id = " + targetSlot, //if all goes well, after that we select a character from the opponent to be attacked
-                        function (error, rows, fields) {
-                            if (error) {
-                                res.send(error);
-                            } else {
-                                res.send(rows);
+        if (alreadyAttacked == true) {
+            res.send("This character already attacked");
+            return;
+        } else {
+            connection.execute("SELECT caracter_id, caracter_attack FROM playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id WHERE player_match_character_player_id = " + playerID + " AND player_match_character_match_id = " + match_id + " AND player_match_character_tile_id = " + attackerSlot, //select a character from player to attack
+                function (error, rows, fields) {
+                    if (error) {
+                        res.send(error);
+                    } else {
+                        if (attackerSlot == 1 || attackerSlot == 2 || attackerSlot == 3) {
+                            if (targetSlot == 4 || targetSlot == 5) {
+                                console.log("Can't attack that target");
+                                return;
                             }
-                        })
-                }
-            })
+                        }
+                        var attackDamage = rows[0].caracter_attack;
+                        console.log("It's: ", alreadyAttacked);
+                        connection.execute("UPDATE playermatchcharacter INNER JOIN caracter ON player_match_character_character_id = caracter_id SET player_match_character_character_current_HP = player_match_character_character_current_HP - " + attackDamage + " WHERE player_match_character_match_id = " + match_id + " AND player_match_character_player_id <> " + playerID + " AND player_match_character_tile_id = " + targetSlot, //if all goes well, after that we select a character from the opponent to be attacked and give him damage
+                            function (error, rows, fields) {
+                                if (error) {
+                                    res.send(error);
+                                } else {
+                                    res.send(rows);
+                                }
+                            })
+                        alreadyAttacked = true;
+                    }
+                })
+            console.log("Now: ", alreadyAttacked);
+        }
     }
 }
 
@@ -64,6 +74,7 @@ router.get('/endTurn', (req, res) => { //ends the turn and passes to the other p
                             res.send(error);
                         } else {
                             res.send(rows);
+                            alreadyAttacked = false;
                         };
                     })
             }
