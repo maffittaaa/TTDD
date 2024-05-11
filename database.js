8296c068050f10b07ff3ce8dc192a7128edec5de
Maffittaa <mafaldam2001@outlook.com>
2024-05-11 13:41:28 +0100

    a

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'v7l.h.filess.io:3307',
+    host: 'v7l.h.filess.io',
     user: 'TTDD2_hungrymay',
     password: '23e62e80afc71206623187f11eade29fd4c80ecd!' ,
-    database: 'TTDD2_hungrymay'
+    database: 'TTDD2_hungrymay',
+    port: 3307
 });
 
 module.exports = connection;
