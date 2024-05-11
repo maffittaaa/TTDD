194ff0919dbb32a65a4ff8020d9519b08950548b
Maffittaa <mafaldam2001@outlook.com>
2024-05-11 13:35:42 +0100

    nb

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
-const os = require('os');
+
 const connection = mysql.createConnection({
-    host: 'v7l.h.filess.io',
+    host: 'v7l.h.filess.io:3307',
     user: 'TTDD2_hungrymay',
     password: '23e62e80afc71206623187f11eade29fd4c80ecd!' ,
     database: 'TTDD2_hungrymay'
